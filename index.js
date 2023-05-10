98ee0c021fd399240dc21706d8c69c3ac7615fda
ariful-24 <ariful.cse.gub@gmail.com>
2023-05-10 23:20:54 +0600

    creating variable in dot notaion

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ const myObject={
     }
 }
 
-console.log(myObject.otherProperty);
-console.log(myObject["obnoxious property"]);
+// console.log(myObject.otherProperty);
+// console.log(myObject["obnoxious property"]);
+
+const variable = 'property'
+console.log(myObject.variable);
+console.log(myObject[variable]);
+
