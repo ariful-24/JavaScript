7ac60d06e2c61a828df7b046f0a636cff88ee551
ariful-24 <ariful.cse.gub@gmail.com>
2023-05-11 00:09:21 +0600

    printing value

--- Object_Constructors.js
+++ Object_Constructors.js
@@ -3,8 +3,14 @@
 function Player(name, marker){
     this.name = name
     this.marker = marker
+    this.sayName = function(){
+        console.log(name)
+    }
     
 }
 
 const playerOne = new Player('arif','X')
 const playerTwo = new Player('owasib','O')
+
+playerOne.sayName()
+playerTwo.sayName()
