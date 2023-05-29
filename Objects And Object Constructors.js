69a54fe0f7ce7d34ce029cd9572904e633ab0352
ariful-24 <ariful.cse.gub@gmail.com>
2023-05-29 19:36:52 +0600

    creating object for player one

--- Objects And Object Constructors.js
+++ Objects And Object Constructors.js
@@ -1,10 +1,20 @@
-const myObject = {
-    property : 'Value',
-    otherProperty: '77',
-    "obnoxious property": function(){
+// const myObject = {
+//     property : 'Value',
+//     otherProperty: '77',
+//     "obnoxious property": function(){
 
-    }
+//     }
+// }
+// const variable = 'property'
+// console.log(myObject.variable);
+// console.log(myObject[variable]); // myObject('property')
+
+const playerOneName = "arif"
+const playerTwoName = "islam"
+const playerOneMarker = "X"
+const palyerTwoMarker = "O"
+
+const playerOne = {
+    name: "arif",
+    marker: "X"
 }
-const variable = 'property'
-console.log(myObject.variable);
-console.log(myObject[variable]); // myObject('property')
