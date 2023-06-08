4d0582fb30fa8caa6bc2bdf9d255e733feec3206
ariful-24 <ariful.cse.gub@gmail.com>
2023-06-08 21:22:31 +0600

    factory function create

--- Factory Functions.js
+++ Factory Functions.js
@@ -1,19 +1,33 @@
 
 
-function Person(name, age){
-    this.name = name;
-    this.age = age;
-}
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
 
-Person.prototype.sayName= function(){
-    console.log(this.name)
-}
+// Person.prototype.sayName= function(){
+//     console.log(this.name)
+// }
 
-Person.prototype.sayAge= function(){
-    console.log(this.age)
-}
+// Person.prototype.sayAge= function(){
+//     console.log(this.age)
+// }
 
-const jeff = new Person('jeff',33);
+// const jeff = new Person('jeff',33);
 
-jeff.sayName();
-jeff.sayAge();
+// jeff.sayName();
+// jeff.sayAge();
+
+
+//Factory function 
+
+const personFactory = (name, age) => {
+    const sayHello = () => console.log('hello!');
+    return { name, age, sayHello };
+  };
+  
+  const arif = personFactory('arif', 27);
+  
+  console.log(arif.name); // 'arif'
+  
+  arif.sayHello(); // calls the function and logs 'hello!'
